c22eb834bd661ff3f846df128d3893e1e7225304
calis307 <camilotero89@gmail.com>
2024-03-01 15:20:28 +0100

    Se actualiza y corrigen bugs

--- src/app/pages/clima/clima.component.ts
+++ src/app/pages/clima/clima.component.ts
@@ -1,28 +1,59 @@
-import { Component} from '@angular/core';
-import { ClimaService } from '../../services/servicio-clima.service';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ClimaService } from '../../services/clima.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-clima',
   templateUrl: './clima.component.html',
   styleUrls: ['./clima.component.css'],
-  imports: [CommonModule,FormsModule],
-  standalone: true
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+})
+export class ClimaComponent  {
+
+  ciudad: string = '';
+  private _climaService = inject(ClimaService);
+  datosClima:any;
+
+
+  buscarCiudad() {
+    this._climaService.buscarClima(this.ciudad).subscribe(
+      (data) => {
+        this.datosClima = this._climaService.procesarDatosClima(data);
+      });
+  }
+ 
+  
+}
+
+
+/* import { Component, inject } from '@angular/core';
+import { ClimaService } from '../../services/clima.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-clima',
+  templateUrl: './clima.component.html',
+  styleUrls: ['./clima.component.css'],
+  imports: [FormsModule,CommonModule],
+  standalone: true,
 })
 export class ClimaComponent {
-  datosClima: any = {};
-  ciudad: string = ''; // Definición de la propiedad ciudad
+
+  ciudad: string = '';
+  datosClima: any;
 
   constructor(private climaService: ClimaService) { }
 
-  buscarClima() {
-    if (this.ciudad) {
-      this.climaService.buscarClima(this.ciudad)
-        .subscribe((data: any) => {
-          this.datosClima = this.climaService.procesarDatosClima(data);
-        });
-    }
+  buscarCiudad() {
+    this.climaService.buscarClima(this.ciudad)
+      .subscribe((data: any) => {
+        this.datosClima = this.climaService.procesarDatosClima(data);
+      });
   }
 }
-
+ */
